f87b4771c3596b56b9a5d576fc7d6fa11990a6ec
Bruno <sampaionascimento100@gmail.com>
2020-10-23 12:03:30 -0300

    Efeitos mouseOver v 1.0

---
+++ scripts/scripts.js
@@ -0,0 +1,55 @@
+
+
+let titulos = document.getElementById("titleMod");
+
+function passarMouseJ(){
+    
+    titulos.innerHTML = "JavaScript ES6+";
+    
+}
+
+function passarMouseN(){
+    
+    titulos.innerHTML = "Node JS";
+    
+}
+
+function passarMouseR(){
+    
+    titulos.innerHTML = "React JS";
+    
+}
+
+function passarMouseH(){
+    
+    titulos.innerHTML = "HTML 5";
+    
+}
+
+function passarMouseC(){
+    
+    titulos.innerHTML = "CSS3";
+    
+}
+
+function passarMouseG(){
+    
+    titulos.innerHTML = "GIT";
+    
+}
+
+function passarMouseGH(){
+    
+    titulos.innerHTML = "GIT HUB";
+    
+}
+
+function passarMouseGP(){
+    
+    titulos.innerHTML = "Gulp";
+    
+}
+
+function tirarMouse(){
+    titulos.innerHTML = "My Skills and Tecnologies"
+}
