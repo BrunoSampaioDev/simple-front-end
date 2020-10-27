1cb4c84f4809e751cca03be33524300698f79b0c
Bruno <sampaionascimento100@gmail.com>
2020-10-26 23:35:09 -0300

    Nome da div trocado

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,7 +10,7 @@ icons.forEach(icon => {
   icon.onmouseover = function() {
     
      // pega o valor dentro do atributo data-conteudo=""
-     const nomeDaDiv = this.dataset.conteudo
+     const dataContent = this.dataset.conteudo
 
      // remove a classe 'visible' de todos elementos
      document.querySelectorAll('.conteudo').forEach(divConteudo => {
@@ -18,7 +18,7 @@ icons.forEach(icon => {
      })
      
      // adiciona a classe 'visible' no conteúdo clicado
-     const conteudo = document.querySelector(`#conteudo_${nomeDaDiv}`)
+     const conteudo = document.querySelector(`#conteudo_${dataContent}`)
      conteudo.classList.add('visible')
 
      //Remove o titulo que aparece antes de iniciar os eventos
